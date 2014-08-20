6aeb31a1e549dbb15289b8293028042ae735b4ea
Zachary Lincoln <zach@burlingtoncollective.com>
2014-08-20 01:17:06 -0400

    functionality for skis and blog page

--- blog.php
+++ blog.php
@@ -3,69 +3,65 @@
 Template Name: Media Page
 */
 include_once('includes/header.php');
+$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+$media = new WP_Query('posts_per_page=5&paged='+$paged);
 ?>
 <section id="blog">
 	<div class="container">
 		<div class="row">
 			<div class="col-xs-8">
+				<?php
+				if($media->have_posts()): while($media->have_posts()): $media->the_post();
+				$categories = wp_get_post_categories(get_the_ID(), array('fields'=>'slugs'));
+				$vimeo_embed = get_post_meta(get_the_ID(), 'vimeo_embed', true);
+				?>
 				<article>
 					<div class="heading">
-						<h3>Title</h3>
+						<h3><?php the_title(); ?></h3>
 						<span class="date">08/08/2014</span>
 					</div>
 					<div class="content">
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam <a href="javascript:void(0)">read more ...</a></p>
-						<img class="img-responsive" src="<?= get_template_directory_uri(); ?>/images/temp/blog-placeholder.png">
+						<div class="wysiwyg-content">
+							<?php the_content(); ?>
+						</div>
+						<!-- <a href="<?php the_permalink(); ?>">read more ...</a> -->
+						<?php
+						if(in_array('video', $categories)): ?>
+						<div class="embed" data-fitvids-target>
+							<?=  $vimeo_embed; ?>
+						</div>
+						<?php
+						elseif(in_array('photo', $categories)): ?>
+						<div class="image-wrapper">
+							<?php the_post_thumbnail('full', array('class'=>'img-responsive')); ?>
+						</div>
+						<?php
+						endif;
+						?>
 					</div>
 				</article>
-				<article>
-					<div class="heading">
-						<h3>Title</h3>
-						<span class="date">08/08/2014</span>
-					</div>
-					<div class="content">
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam <a href="javascript:void(0)">read more ...</a></p>
-						<img class="img-responsive" src="<?= get_template_directory_uri(); ?>/images/temp/blog-placeholder.png">
-					</div>
-				</article>
-				<article>
-					<div class="heading">
-						<h3>Title</h3>
-						<span class="date">08/08/2014</span>
-					</div>
-					<div class="content">
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam <a href="javascript:void(0)">read more ...</a></p>
-						<img class="img-responsive" src="<?= get_template_directory_uri(); ?>/images/temp/blog-placeholder.png">
-					</div>
-				</article>
+				<?php
+				endwhile; endif;
+				?>
 				<ul class="pager">
-				  <li class="previous"><a href="#">&larr; Older</a></li>
-				  <li class="next"><a href="#">Newer &rarr;</a></li>
+				  <li class="previous"><?php previous_posts_link( '&larr; Previous Entries' ); ?></li>
+				  <li class="next"><?php next_posts_link( 'Next Entries &rarr;', '' ); ?></li>
 				</ul>
 			</div>
 			<div id="sidebar" class="col-xs-3 col-xs-push-1">
 				<img class="img-responsive ad" src="<?= get_template_directory_uri(); ?>/images/temp/ad-placeholder.png">
-				<h4>Cateories</h4>
+				<h4>Categories</h4>
 				<ul class="nav nav-stacked">
-					<li><a href="javascript:void(0)">Video</a></li>
-					<li><a href="javascript:void(0)">Trips</a></li>
-					<li><a href="javascript:void(0)">Product News</a></li>
-					<li><a href="javascript:void(0)">Team News</a></li>
-					<li><a href="javascript:void(0)">Photos</a></li>
+					<?php
+					$videoCat = get_category_by_slug('video');
+					$photoCat = get_category_by_slug('photo');
+					?>
+					<li><a href="<?= get_category_link($videoCat->term_id); ?>"><?= $videoCat->name; ?></a></li>
+					<li><a href="<?= get_category_link($photoCat->term_id); ?>"><?= $photoCat->name; ?></a></li>
 				</ul>
 				<h4>Archive</h4>
 				<ul class="nav nav-stacked">
-					<li><a href="javascript:void(0)">August 2014</a></li>
-					<li><a href="javascript:void(0)">July 2014</a></li>
-					<li><a href="javascript:void(0)">June 2014</a></li>
-					<li><a href="javascript:void(0)">May 2014</a></li>
-					<li><a href="javascript:void(0)">April 2014</a></li>
-					<li><a href="javascript:void(0)">March 2014</a></li>
-					<li><a href="javascript:void(0)">February 2014</a></li>
-					<li><a href="javascript:void(0)">January 2014</a></li>
+					<?php wp_get_archives(array('limit'=>8)); ?>
 				</ul>
 			</div>
 		</div>
--- skis.php
+++ skis.php
@@ -3,6 +3,7 @@
 Template Name: Skis Page
 */
 include_once('includes/header.php');
+if (have_posts()) : while (have_posts()) : the_post();
 ?>
 <section class="heading">
 	<div class="container">
@@ -33,12 +34,9 @@ include_once('includes/header.php');
 			</div>
 			<div class="col-xs-6">
 				<div class="description-wrapper">
-					<h2>Stinger</h2>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-					<span class="price">$599</span>
-					<form>
-						<button class="btn btn-lg btn-primary" type="submit">Buy Now</button>
-					</form>
+					<h2><?php the_title(); ?></h2>
+					<?php the_content(); ?>
+					<?= get_post_meta(get_the_ID(), 'ski_paypal_embed', true); ?>
 					<h3>Dimensions</h3>
 					<table class="table table-striped">
 						<tbody>
@@ -144,5 +142,6 @@ include_once('includes/header.php');
 	</div>
 </section>
 <?php
+endwhile; endif;
 include_once('includes/footer.php');
 ?>
