8af4d40f540cad34708db68060b36a6a5f1148db
Zachary Lincoln <zach@burlingtoncollective.com>
2014-08-20 21:49:01 -0400

    company page additions

--- about.php
+++ about.php
@@ -20,14 +20,14 @@ include_once('includes/header.php');
 	<div class="container">
 		<div class="row">
 			<div class="col-xs-3">
-				<img class="img-responsive" src="<?= get_template_directory_uri(); ?>/images/smoke-polaroid.png" style="margin: 0 auto;">
+				<img class="img-responsive" src="<?= get_template_directory_uri(); ?>/images/jim-gaeta-gibson.png" style="margin: 0 auto;">
 			</div>
 			<div class="col-xs-6">
 				<p style="margin-top: 40px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 				<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 			</div>
 			<div class="col-xs-3">
-				<img class="img-responsive" src="<?= get_template_directory_uri(); ?>/images/smoke-polaroid.png" style="margin: 0 auto;">
+				<img class="img-responsive" src="<?= get_template_directory_uri(); ?>/images/the-genny-struggle.png" style="margin: 0 auto;">
 			</div>
 		</div>
 	</div>
@@ -99,14 +99,14 @@ include_once('includes/header.php');
 		<div class="row">
 			<div class="col-xs-12">
 				<p>
-					<img src="<?= get_template_directory_uri(); ?>/images/history-polaroids/top.png" style="float: right; margin: 0px -40px 0 0;"><img src="<?= get_template_directory_uri(); ?>/images/history-polaroids/middle.png" style="float: right; margin: 0px -40px 0 0;"><img src="<?= get_template_directory_uri(); ?>/images/history-polaroids/bottom.png" style="float: right; margin: 0px -40px 0 0;">
-					HG Skis is a freestyle ski company based in Burlington, VT. While we’ve been building and designing skis since 2006, the company was officially established in 2010. In the age of “locally grown products” and “micro-brew” ski companies, the east coast has been left without its own ski brand. As the only representatives of the east coast in the ski industry, we design, prototype, and manufacture product specifically for our terrain.<br><br>
+					<img src="<?= get_template_directory_uri(); ?>/images/history-polaroids/top-alt.png" style="float: right; margin: 0px -40px 0 0;"><img src="<?= get_template_directory_uri(); ?>/images/history-polaroids/middle-alt.png" style="float: right; margin: 0px -40px 0 0;"><img src="<?= get_template_directory_uri(); ?>/images/history-polaroids/bottom-alt.png" style="float: right; margin: 0px -40px 0 0;">
+					HG Skis is a freestyle ski company based in Burlington, VT. While we've been building and designing skis since 2006, the company was officially established in 2010. In the age of "locally grown products" and "micro-brew" ski companies, the east coast has been left without its own ski brand. As the only representatives of the east coast in the ski industry, we design, prototype, and manufacture product specifically for our terrain.<br><br>
 					In 2006 I began work on a senior project in high school. Over a five 
 					month process I built a ski press, a mold, and many of the 
 					specialized ski tools still used by the company today. 
 					Above all I built a pair of skis that I am still in love with.<br><br>
 					For my first two years of college I was in an out of other 
-					peoples’ shop space. It was not until my go-to shop told me that I wasn’t allowed back that I decided to invest in my own machines, and start a ski company. During my third year of college I befriend Connor Gaeta. Connor originally started with me to develop our first twin tip, and function as a pro.<br><br>
+					peoples' shop space. It was not until my go-to shop told me that I wasn’t allowed back that I decided to invest in my own machines, and start a ski company. During my third year of college I befriend Connor Gaeta. Connor originally started with me to develop our first twin tip, and function as a pro.<br><br>
 					Since then, Connor has developed into a major component of 
 					the company. The two of us are very different people, but we share one major thing in common. We are crazy about skiing, and want to bring innovation to the sport in anyway possible.<br><br>
 					Currently we work out of a garage in Burlington, Vermont. We live in your neighborhood, and we are building our favorite skis for you.<br><br>
@@ -131,7 +131,7 @@ include_once('includes/header.php');
 	<div class="container">
 		<div class="row">
 			<div class="col-xs-3">
-				<img class="img-responsive" src="<?= get_template_directory_uri(); ?>/images/first-stop-polaroid.png">
+				<img class="img-responsive" src="<?= get_template_directory_uri(); ?>/images/board-barn.png">
 			</div>
 			<div class="col-xs-3">
 				<h3>First Stop Board Barn</h3>
@@ -141,13 +141,13 @@ include_once('includes/header.php');
 				</ul>
 			</div>
 			<div class="col-xs-3">
-				<img class="img-responsive" src="<?= get_template_directory_uri(); ?>/images/first-stop-polaroid.png">
+				<img class="img-responsive" src="<?= get_template_directory_uri(); ?>/images/alpine-shop.png">
 			</div>
 			<div class="col-xs-3">
-				<h3>First Stop Board Barn</h3>
+				<h3>Alpine Shop</h3>
 				<ul class="address">
-					<li>8474 Rt. 4</li>
-					<li>Killington, VT USA 05751</li>
+					<li>1184 Williston Rd.</li>
+					<li>South Burlington, VT USA 05403</li>
 				</ul>
 			</div>
 		</div>
@@ -158,7 +158,7 @@ include_once('includes/header.php');
 		<div class="row">
 			<div class="col-xs-12">
 				<div class="page-heading">
-					<h1>Warrenty</h1>
+					<h1>Warranty</h1>
 				</div>
 			</div>
 		</div>
