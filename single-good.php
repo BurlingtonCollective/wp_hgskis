df7e9ec2dba70a4cc917729e725d2c7f73629ca3
Zachary Lincoln <zach@daftlabs.com>
2015-02-08 18:36:55 -0500

    goods single updates

--- single-good.php
+++ single-good.php
@@ -21,6 +21,9 @@ if (have_posts()) : while (have_posts()) : the_post();
 				<?php
 				$mainImg = types_render_field('main-image', array('class'=>'img-responsive'));
 				$altImg = types_render_field('alt-good-view', array('class'=>'img-responsive show-hover'));
+				$slideshowImagesRaw = types_render_field('good-slide-image', array('output'=>'raw'));
+				$slideshowImages = explode(' ', $slideshowImagesRaw);
+				$vimeoEmbed = types_render_field('good-vimeo-embed', array('output'=>'raw'));
 				?>
 				<div class="product-wrapper <?= !empty($altImg) ? 'switcheroo' : '';?>">
 					<?php if(!empty($altImg)):?>
@@ -43,6 +46,53 @@ if (have_posts()) : while (have_posts()) : the_post();
 						echo types_render_field('additional-information', array());
 						?>
 					</div>
+					<?php if(!empty($vimeoEmbed)): ?>
+					<div class="embed" data-fitvids-target>
+						<?= $vimeoEmbed; ?>
+					</div>
+					<?php
+					endif;
+					?>
+					<?php if(!empty($slideshowImagesRaw)): ?>
+					<div id="good-carousel" class="carousel slide">
+						<ol class="carousel-indicators">
+					  	<?php
+					  	$indicatorActive = true;
+					  	$indicatorIndex = 0;
+					  	while($indicatorIndex <= count($slideshowImages)-1):
+					  	?>
+					    <li data-target="#good-carousel" data-slide-to="<?= $indicatorIndex; ?>" class="<?= $indicatorActive ? 'active' : '';?>"></li>
+					    <?php
+					    $indicatorIndex++;
+					    $indicatorActive = false;
+					    endwhile;
+					    ?>
+					  </ol>
+					  <div class="carousel-inner">
+						  <?php
+						  $slideActive = true;
+						  $slideIndex = 0;
+						  while($slideIndex <= count($slideshowImages)-1):
+						  ?>
+							<div class="item <?= $slideActive ? 'active' : ''; ?>">
+								<img class="img-responsive" src="<?= $slideshowImages[$slideIndex]; ?>">
+							</div>
+							<?php
+							$slideIndex++;
+							$slideActive = false;
+							endwhile;
+							?>
+					  </div>
+
+					  <!-- Controls -->
+					  <a class="left carousel-control" href="#good-carousel" role="button" data-slide="prev">
+					    <span></span>
+					  </a>
+					  <a class="right carousel-control" href="#good-carousel" role="button" data-slide="next">
+					    <span></span>
+					  </a>
+					</div>
+					<?php endif; ?>
 				</div>
 			</div>
 		</div>
