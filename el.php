e0767c1e7057560fa584eb722062b05e63e53ac8
zlincoln <znlincoln@gmail.com>
2015-05-21 21:53:22 -0400

    adapting edits to el template

--- el.php
+++ el.php
@@ -9,7 +9,7 @@ if (have_posts()) : while (have_posts()) : the_post();
 	<div class="container">
 		<div class="row">
 			<div class="col-xs-12">
-				<h1>Powder Ski</h1>
+				<h1>Rockered Powder Ski</h1>
 			</div>
 		</div>
 	</div>
@@ -18,8 +18,15 @@ if (have_posts()) : while (have_posts()) : the_post();
 	<div class="container">
 		<div class="row">
 			<div class="col-xs-12 col-sm-6">
-				<div class="product-wrapper">
-					<img class="img-responsive" src="http://hgskis.com/wp-content/uploads/2015/04/El.jpg">
+				<div class="row product-wrapper">
+					<div class="col-xs-6">
+						<img class="img-responsive" src="<?= get_template_directory_uri(); ?>/images/topsheet.png">
+						<img class="img-responsive" src="<?= get_template_directory_uri(); ?>/images/el/topsheet.png">
+					</div>
+					<div class="col-xs-6">
+						<img class="img-responsive" src="<?= get_template_directory_uri(); ?>/images/base.png">
+						<img class="img-responsive" src="<?= get_template_directory_uri(); ?>/images/el/base.png">
+					</div>
 				</div>
 			</div>
 			<div class="col-xs-12 col-sm-6">
@@ -27,7 +34,7 @@ if (have_posts()) : while (have_posts()) : the_post();
 					<h2>EL</h2>
 					<p>Most powder skis were designed for Alaska with huge open lines and neck deep snow.  We've developed a ski for the rest of us.  One which is just as floaty, but nimble enough to navigate the tightest tree runs.  The EL has a tight sidecut that lets you bring the ski onto groomers without noticing you're on a ski designed with powder in mind.  We recommend bringing the EL everywhere, from east coast glades to west coast big mountain backcountry.  The EL has all the same durability and carving technologies as our other skis.</p>
 					<span class="price">$450.00</span>
-					<form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+					<form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post" style="margin-bottom: 40px;">
 						<input type="hidden" name="cmd" value="_s-xclick">
 						<input type="hidden" name="hosted_button_id" value="YENRL7A2CWQAW">
 						<table>
