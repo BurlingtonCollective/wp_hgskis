21e2b44b0efdd56be5c64315557e035a4739a4d1
zlincoln <znlincoln@gmail.com>
2015-09-16 14:40:19 -0400

    added spitfire model to skis page

--- preorder-2015-skis.php
+++ preorder-2015-skis.php
@@ -19,15 +19,21 @@ if (have_posts()) : while (have_posts()) : the_post();
 		<div class="row">
 			<div class="col-md-6">
 				<div class="wrapper wrapper-left">
-					<h2 style="text-align: right; font-size: 22px; line-height: 37px;"><img class="img-responsive pull-left" src="<?= get_template_directory_uri(); ?>/images/stinger/title-logo.png" alt="Stinger">Full Camber Park Ski</h2>
+					<h2 style="text-align: right; font-size: 18px;"><img class="img-responsive pull-left" src="<?= get_template_directory_uri(); ?>/images/stinger/title-logo.png" alt="Stinger"><span style="display: block; font-size: 12px; line-height: 14px; margin-bottom: 6px;">Subway Stinger</span>Full Camber Park Ski (178cm)</h2>
 					<a href="http://hgskis.com/?page_id=2338" style="display: block; padding-top: 20px;">
 						<img class="img-responsive" src="<?= get_template_directory_uri(); ?>/images/stinger/2015/topsheet-horz.png">
+					</a>
+				</div>
+				<div class="wrapper wrapper-left">
+					<h2 style="text-align: right; font-size: 18px;"><img class="img-responsive pull-left" src="<?= get_template_directory_uri(); ?>/images/stinger/title-logo.png" alt="Stinger"><span style="display: block; font-size: 12px; line-height: 14px; margin-bottom: 6px;">Spitfire Stinger</span>Full Camber Park Ski (169cm)</h2>
+					<a href="http://hgskis.com/?page_id=2839" style="display: block; padding-top: 20px;">
+						<img class="img-responsive" src="<?= get_template_directory_uri(); ?>/images/stinger/topsheet-horz.png">
 					</a>
 				</div>
 			</div>
 			<div class="col-md-6">
 				<div class="wrapper wrapper-right">
-					<h2 style="text-align: right; font-size: 22px; line-height: 37px;"><img class="img-responsive pull-left" style="margin-top: -20px;" src="<?= get_template_directory_uri(); ?>/images/el/title-logo.png" alt="EL">Rockered Powder Ski</h2>
+					<h2 style="text-align: right; font-size: 18px; line-height: 37px;"><img class="img-responsive pull-left" style="margin-top: -20px;" src="<?= get_template_directory_uri(); ?>/images/el/title-logo.png" alt="EL">Rockered Powder Ski (182cm)</h2>
 					<a href="http://hgskis.com/?page_id=2815" style="display: block; padding-top: 20px;">
 						<img class="img-responsive" src="<?= get_template_directory_uri(); ?>/images/el/topsheet-horz.png">
 					</a>
