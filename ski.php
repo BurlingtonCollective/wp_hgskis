0a1f186b6b9a5d9515fec47a2d0e003d7c635709
Zachary Lincoln <znlincoln@gmail.com>
2015-12-21 01:58:01 -0500

    single ski template wiredup

---
+++ ski.php
@@ -0,0 +1,292 @@
+<?php
+include_once('includes/header.php');
+if (have_posts()) : while (have_posts()) : the_post();
+    $ski = new stdObject();
+    $ski->name = get_the_title();
+    $ski->description = get_the_content();
+
+    $ski->logo = types_render_field('ski-logo', array('output'=>'raw'));
+    $ski->topSheetImage = types_render_field('ski-top-sheet-image', array('output'=>'raw'));
+    $ski->baseImage = types_render_field('ski-base-image', array('output'=>'raw'));
+    $ski->horzImage = types_render_field('ski-horz-image', array('output'=>'raw'));
+    $ski->showVimeoEmbed = types_render_field('ski-show-vimeo-embed', array('output'=>'raw'));
+    $ski->vimeoEmbed = types_render_field('ski-vimeo-embed', array('output'=>'raw'));
+
+    $ski->shipDateKnown = boolval(types_render_field('ski-ship-date-known', array('output'=>'raw')));
+    $ski->expectedShipDate = types_render_field('ski-expected-ship-date', array('style'=>'text', 'format'=>'F j'));
+    $ski->freeShipping = boolval(types_render_field('ski-free-shipping', array('output'=>'raw')));
+
+    $ski->currentPrice = types_render_field('ski-current-price', array('output'=>'raw'));
+    $ski->isPreorder = boolval(types_render_field('ski-is-preorder', array('output'=>'raw')));
+    $ski->showMSRP = boolval(types_render_field('ski-show-msrp', array('output'=>'raw')));
+    $ski->msrpPrice = types_render_field('ski-msrp-price', array('output'=>'raw'));
+    $ski->paypalForm = types_render_field('ski-paypal-form', array('output'=>'row'));
+
+    $ski->typeTagline = types_render_field('ski-type-typeline', array('output'=>'raw'));
+    $ski->displayLength = types_render_field('ski-display-length', array('output'=>'raw'));
+    $ski->showDimensions = boolval(types_render_field('ski-show-dimensions', array('output'=>'raw')));
+    $ski->skiDimensionsImage = types_render_field('ski-dimensions-image', array('output'=>'raw'));
+    $ski->showTechnologyPanel = boolval(types_render_field('ski-show-technology-panel', array('output'=>'raw')));
+    $ski->skowReviewsImage = boolval(types_render_field('ski-show-reviews-image', array('output'=>'raw')));
+    $ski->reviewsImage = types_render_field('ski-reviews-image', array('output'=>'raw'));
+
+    $ski->showAd = boolval(types_render_field('ski-show-ad', array('output'=>'raw')));
+    $ski->adImage = types_render_field('ski-ad-image', array('output'=>'raw'));
+    $ski->adLink = types_render_field('ski-ad-link', array('output'=>'raw'));
+?>
+<section class="heading">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <h1><?= $ski->typeTagline; ?></h1>
+            </div>
+        </div>
+    </div>
+</section>
+<section id="product">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <div class="row product-wrapper">
+                    <div class="col-xs-6">
+                        <img class="img-responsive" src="<?= get_template_directory_uri(); ?>/images/topsheet.png">
+                        <img class="img-responsive" src="<?= $ski->topSheetImage; ?>">
+                    </div>
+                    <div class="col-xs-6">
+                        <img class="img-responsive" src="<?= get_template_directory_uri(); ?>/images/base.png">
+                        <img class="img-responsive" src="<?= $ski->baseImage; ?>">
+                    </div>
+                </div>
+                <div class="hidden-xs" data-fitvids-target>
+                    <?php if ($ski->showVimeoEmbed) : ?>
+                        <?= $ski->vimeoEmbed; ?>
+                    <?php endif; ?>
+                    <?php if ($ski->showAd) : ?>
+                        <div>
+                            <a href="<?= $ski->adLink; ?>">
+                                <img class="img-responsive" src="<?= $ski->adImage; ?>">
+                            </a>
+                        </div>
+                    <?php endif; ?>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <div class="description-wrapper">
+                    <h2><?= $ski->name; ?></h2>
+                    <?= $ski->description; ?>
+                    <?php if ($ski->freeShipping) : ?>
+                        <span class="free-shipping">Free Shipping</span>
+                    <?php endif; ?>
+                    <span class="price">
+                        <?php if ($ski->isPreorder) : ?>
+                            <span class="preorder">Preorder: <?= $ski->currentPrice; ?></span>
+                        <?php else : ?>
+                            <?= $ski->currentPrice; ?>
+                        <?php endif; ?>
+                        <?php if ($ski->showMSRP) : ?>
+                            (MSRP <?= $ski->msrpPrice; ?>)
+                        <?php endif; ?>
+                    </span>
+                    <?php if ($ski->shipDateKnown) : ?>
+                        <span class="expected-ship-date">Expected Ship Date: <?= $ski->expectedShipDate; ?></span>
+                    <?php endif; ?>
+
+                    <?= $ski->paypalForm; ?>
+
+                    <?php if ($ski->showDimensionsImage) : ?>
+                        <h3>Dimensions</h3>
+                        <img class="img-responsive" src="<?= $ski->dimensionsImage; ?>">
+                    <?php endif; ?>
+                    <?php if ($ski->showTechnologyPanel) : ?>
+                        <h3>Technology</h3>
+                        <div class="panel-group" id="technology">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h4 class="panel-title">
+                                        <a href="#panel-0" class="open" data-toggle="collapse" data-parent="#technology">
+                                            <img class="img-responsive initial" src="<?= get_template_directory_uri();?>/images/stinger/techicons/flexin.png"><img class="img-responsive show-hover" src="<?= get_template_directory_uri();?>/images/stinger/techicons/flexin-rollover.png">
+                                            <span>Popeye Flex</span>
+                                            <i class="glyphicon glyphicon-chevron-down"></i>
+                                            <i class="glyphicon glyphicon-chevron-right"></i>
+                                        </a>
+                                    </h4>
+                                </div>
+                                <div class="panel-collapse collapse in" id="panel-0">
+                                    <div class="panel-body">
+                                        <ul>
+                                            <li>Balanced flex profile for maximum control.</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h4 class="panel-title">
+                                        <a href="#panel-7" data-toggle="collapse" data-parent="#technology">
+                                            <img class="img-responsive initial" src="<?= get_template_directory_uri(); ?>/images/stinger/techicons/pop.png"><img class="img-responsive show-hover" src="<?= get_template_directory_uri(); ?>/images/stinger/techicons/pop-rollover.png">
+                                            <span>Champagne Pop</span>
+                                            <i class="glyphicon glyphicon-chevron-down"></i>
+                                            <i class="glyphicon glyphicon-chevron-right"></i>
+                                        </a>
+                                    </h4>
+                                </div>
+                                <div class="panel-collapse collapse" id="panel-7">
+                                    <div class="panel-body">
+                                        <ul>
+                                            <li>More pop than a bottle of grand vintage on Birdman's birthday.</li>
+                                            <li>Our camber will get you higher.</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h4 class="panel-title">
+                                        <a href="#panel-1" data-toggle="collapse" data-parent="#technology">
+                                            <img class="img-responsive initial" src="<?= get_template_directory_uri();?>/images/stinger/techicons/mona.png"><img class="img-responsive show-hover" src="<?= get_template_directory_uri();?>/images/stinger/techicons/mona-rollover.png">
+                                            <span>Art Attack</span>
+                                            <i class="glyphicon glyphicon-chevron-down"></i>
+                                            <i class="glyphicon glyphicon-chevron-right"></i>
+                                        </a>
+                                    </h4>
+                                </div>
+                                <div class="panel-collapse collapse" id="panel-1">
+                                    <div class="panel-body">
+                                        <ul>
+                                            <li>People will line up to see your skis.</li>
+                                            <li>Hand painted graphic from skiers like you.</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h4 class="panel-title">
+                                        <a href="#panel-2" data-toggle="collapse" data-parent="#technology">
+                                            <img class="img-responsive initial" src="<?= get_template_directory_uri();?>/images/stinger/techicons/sidewall.png"><img class="img-responsive show-hover" src="<?= get_template_directory_uri();?>/images/stinger/techicons/sidewall-rollover.png">
+                                            <span>8th Wonder Sidewall</span>
+                                            <i class="glyphicon glyphicon-chevron-down"></i>
+                                            <i class="glyphicon glyphicon-chevron-right"></i>
+                                        </a>
+                                    </h4>
+                                </div>
+                                <div class="panel-collapse collapse" id="panel-2">
+                                    <div class="panel-body">
+                                        <ul>
+                                            <li>Side wall geometry that holds edge pressure, not snow buildup.</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h4 class="panel-title">
+                                        <a href="#panel-3" data-toggle="collapse" data-parent="#technology">
+                                            <img class="img-responsive initial" src="<?= get_template_directory_uri();?>/images/stinger/techicons/no-delam.png"><img class="img-responsive show-hover" src="<?= get_template_directory_uri();?>/images/stinger/techicons/no-delam-rollover.png">
+                                            <span>Zero-Delam</span>
+                                            <i class="glyphicon glyphicon-chevron-down"></i>
+                                            <i class="glyphicon glyphicon-chevron-right"></i>
+                                        </a>
+                                    </h4>
+                                </div>
+                                <div class="panel-collapse collapse" id="panel-3">
+                                    <div class="panel-body">
+                                        <ul>
+                                            <li>Your tips won't explode when you get to know the gucci plateau.</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h4 class="panel-title">
+                                        <a href="#panel-4" data-toggle="collapse" data-parent="#technology">
+                                            <img class="img-responsive initial" src="<?= get_template_directory_uri();?>/images/stinger/techicons/base.png"><img class="img-responsive show-hover" src="<?= get_template_directory_uri();?>/images/stinger/techicons/base-rollover.png">
+                                            <span>Double Base</span>
+                                            <i class="glyphicon glyphicon-chevron-down"></i>
+                                            <i class="glyphicon glyphicon-chevron-right"></i>
+                                        </a>
+                                    </h4>
+                                </div>
+                                <div class="panel-collapse collapse" id="panel-4">
+                                    <div class="panel-body">
+                                        <ul>
+                                            <li>Party on Wayne.</li>
+                                            <li>Our bases are double the industry standard thinkness, twice as thick and twice as strong.</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h4 class="panel-title">
+                                        <a href="#panel-5" data-toggle="collapse" data-parent="#technology">
+                                            <img class="img-responsive initial" src="<?= get_template_directory_uri();?>/images/stinger/techicons/fatty-edges.png"><img class="img-responsive show-hover" src="<?= get_template_directory_uri();?>/images/stinger/techicons/fatty-edges-rollover.png">
+                                            <span>Fat Ass Edges</span>
+                                            <i class="glyphicon glyphicon-chevron-down"></i>
+                                            <i class="glyphicon glyphicon-chevron-right"></i>
+                                        </a>
+                                    </h4>
+                                </div>
+                                <div class="panel-collapse collapse" id="panel-5">
+                                    <div class="panel-body">
+                                        <ul>
+                                            <li>Twice the industry standard thinkness, so you can slam em' hard.</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h4 class="panel-title">
+                                        <a href="#panel-8" data-toggle="collapse" data-parent="#technology">
+                                            <img class="img-responsive initial" src="<?= get_template_directory_uri();?>/images/stinger/techicons/early-edge.png"><img class="img-responsive show-hover" src="<?= get_template_directory_uri();?>/images/stinger/techicons/early-edge-rollover.png">
+                                            <span>Early End Edge</span>
+                                            <i class="glyphicon glyphicon-chevron-down"></i>
+                                            <i class="glyphicon glyphicon-chevron-right"></i>
+                                        </a>
+                                    </h4>
+                                </div>
+                                <div class="panel-collapse collapse" id="panel-8">
+                                    <div class="panel-body">
+                                        <ul>
+                                            <li>Our edges end below the contact point of the ski.  This reduces pressure in the tips, preventing edge blowouts.</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    <h4 class="panel-title">
+                                        <a href="#panel-6" data-toggle="collapse" data-parent="#technology">
+                                            <img class="img-responsive initial" src="<?= get_template_directory_uri();?>/images/stinger/techicons/symmetrical.png"><img class="img-responsive show-hover" src="<?= get_template_directory_uri();?>/images/stinger/techicons/symmetrical-rollover.png">
+                                            <span>Symmetric Design</span>
+                                            <i class="glyphicon glyphicon-chevron-down"></i>
+                                            <i class="glyphicon glyphicon-chevron-right"></i>
+                                        </a>
+                                    </h4>
+                                </div>
+                                <div class="panel-collapse collapse" id="panel-6">
+                                    <div class="panel-body">
+                                        <ul>
+                                            <li>It's fun to ski backwards.</li>
+                                            <li>Fully symmetric ski - feels like freedom.</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    <?php endif; ?>
+                    <?php if ($ski->showReviewsImage) : ?>
+                        <h3>Reviews</h3>
+                        <img class="img-responsive" src="<?= $ski->reviewsImage; ?>">
+                    <?php endif; ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?php
+endwhile; endif;
+include_once('includes/footer.php');
+?>
